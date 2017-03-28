96a45c7274422ecee6323e7058bd3dd00a31a2c9
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-03-28 17:27:31 +0800

    Library an extension of extended map

--- src/server/library/index.ts
+++ src/server/library/index.ts
@@ -1,6 +1,7 @@
+import { ExtendedMap } from '../helpers'
 import AutoIncrement from './auto-increment'
 
-export default class Library<T> extends Map<number, T> {
+export default class Library<T> extends ExtendedMap<number, T> {
     readonly ai = new AutoIncrement()
 
     add(value: T, key = this.ai.nextValue) {
@@ -12,27 +13,5 @@ export default class Library<T> extends Map<number, T> {
         this.ai.setNewCurrent(key)
         super.set(key, value)
         return this
-    }
-
-    *search(iterator: (v: T, k: number) => boolean): IterableIterator<[number, T]> {
-        for (let [key, value] of this) {
-            if (iterator(value, key)) {
-                yield [key, value]
-            }
-        }
-    }
-
-    *map<U>(iterator: (v: T, i: number) => U) {
-        for (let [key, value] of this) {
-            yield iterator(value, key)
-        }
-    }
-
-    get random(): [number, T] {
-        var keys = [...this.keys()]
-        var keyIndex = Math.floor(Math.random() * keys.length)
-        var getKey = keys[keyIndex]
-
-        return [getKey, this.get(getKey)]
     }
 }
