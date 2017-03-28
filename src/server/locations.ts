7e03fd9658151e8cf36a44a88f0a643f5bf074cf
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-03-28 16:25:03 +0800

    Added location  roles * Airplane * Bank * Beach * Cathedral * Casino * Corporate Party * Crusader Army * Day Spa

--- src/server/locations.ts
+++ src/server/locations.ts
@@ -10,15 +10,87 @@ class Location {
 const locations = new Library<Location>()
 export default locations
 
-locations.add(new Location('Airplane'))
-locations.add(new Location('Bank'))
-locations.add(new Location('Beach'))
-locations.add(new Location('Cathedral'))
-locations.add(new Location('Casino'))
-locations.add(new Location('Circus Tent'))
-locations.add(new Location('Corporate Party'))
-locations.add(new Location('Crusader Army'))
-locations.add(new Location('Day Spa'))
+locations.add(new Location('Airplane', [
+    '1st Class Passenger',
+    'Air Marshal',
+    'Mechanic',
+    'Coach Passenger',
+    'Flight Attendant',
+    'Co-Pilot',
+    'Captain'
+]))
+locations.add(new Location('Bank', [
+    'Armored Car Driver',
+    'Bank Manager',
+    'Loan Consultant',
+    'Bank Robber',
+    'Customer',
+    'Security Guard',
+    'Bank Teller'
+]))
+locations.add(new Location('Beach', [
+    'Beach Bartender',
+    'Kite Surfer',
+    'Lifeguard',
+    'Thief',
+    'Beach Goer',
+    'Beach Photographer',
+    'Ice Cream Man'
+]))
+locations.add(new Location('Cathedral', [
+    'Priest',
+    'Beggar',
+    'Sinner',
+    'Parishioner',
+    'Tourist',
+    'Deacon',
+    'Choir Singer'
+]))
+locations.add(new Location('Casino', [
+    'Bartender',
+    'Head of Security',
+    'Bouncer',
+    'Pit Boss',
+    'Hustler',
+    'Dealer',
+    'Gambler'
+]))
+locations.add(new Location('Circus Tent', [
+    'Acrobat',
+    'Animal Trainer',
+    'Magician',
+    'Audience Member',
+    'Fire Eater',
+    'Clown',
+    'Juggler'
+]))
+locations.add(new Location('Corporate Party', [
+    'Enterntainer',
+    'Manager',
+    'Party Crasher',
+    'Owner/CEO',
+    'Admin Assistant',
+    'Accountant',
+    'Delivery Boy'
+]))
+locations.add(new Location('Crusader Army', [
+    'Monk',
+    'Imprisoned Arab',
+    'Servant',
+    'Bishop',
+    'Squire',
+    'Archer',
+    'Knight'
+]))
+locations.add(new Location('Day Spa', [
+    'Customer',
+    'Stylist',
+    'Massage Tech',
+    'Manicurist',
+    'Makeup Artist',
+    'Dematologist',
+    'Beautician'
+]))
 
 locations.add(new Location('Embassy'))
 locations.add(new Location('Hospital'))
