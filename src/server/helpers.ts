3db453482974a4897e3bf3c4912d5ec56f19481c
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-03-28 15:11:32 +0800

    Refactor mergeObj function

--- src/server/helpers.ts
+++ src/server/helpers.ts
@@ -1,14 +1,6 @@
 
 export function mergeObj<A, B>(argsa: A, argsb: B): A & B {
-    var res = <A & B>{}
-
-    for (let a in argsa) {
-        if (typeof argsa[a] == 'function') {
-            (<A>res)[a] = (<any>argsa[a]).bind(res)
-        } else {
-            (<A>res)[a] = argsa[a]
-        }
-    }
+    var res = <A & B>argsa
 
     for (let a in argsb) {
         if (typeof argsb[a] == 'function') {
