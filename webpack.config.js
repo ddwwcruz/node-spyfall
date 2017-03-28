5f2c7717814aae3170cbf3455f61b93193e78c2e
Wayne Dela Cruz <ddwwcruz@gmail.com>
2017-03-28 12:56:41 +0800

    Create webpack configurations

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    context: path.resolve(__dirname, './src/cli'),
+    entry: './index.tsx',
+    output: {
+        filename: 'index.js',
+        path: `${__dirname}/build/cli`
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [
+            '.ts',
+            '.tsx',
+            '.js',
+            '.jsx',
+            '.json'
+        ],
+    },
+    module: {
+        rules: [{
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /src\/server/
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+}
